20688079fb0429971596055cbd1e4db8cc0c7829
Aaron Hardy <athardy937@gmail.com>
2024-12-02 17:22:25 -0500

    First commit. Modified schema.sql and data.sql

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,32 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO categories (category_name) VALUES ('Breakfast');
+INSERT INTO categories (category_name) VALUES ('Lunch');
+INSERT INTO categories (category_name) VALUES ('Dinner');
+INSERT INTO categories (category_name) VALUES ('Snacks');
+INSERT INTO categories (category_name) VALUES ('Desert');
+INSERT INTO categories (category_name) VALUES ('Vegetarian');
+INSERT INTO categories (category_name) VALUES ('Vegan');
+INSERT INTO categories (category_name) VALUES ('Low-Carb');
+INSERT INTO categories (category_name) VALUES ('Paleo');
+INSERT INTO categories (category_name) VALUES ('Keto');
+
+INSERT INTO ingredient_type (type_name) VALUES ('Herb');
+INSERT INTO ingredient_type (type_name) VALUES ('Meat');
+INSERT INTO ingredient_type (type_name) VALUES ('Vegetable');
+INSERT INTO ingredient_type (type_name) VALUES ('Fruit');
+INSERT INTO ingredient_type (type_name) VALUES ('Dairy');
+INSERT INTO ingredient_type (type_name) VALUES ('Cheese');
+INSERT INTO ingredient_type (type_name) VALUES ('Grain');
+INSERT INTO ingredient_type (type_name) VALUES ('Other');
+
+INSERT INTO ingredients (ingredient_name, type_id) VALUES ('Tomato', (SELECT type_id FROM ingredient_type WHERE name = 'Fruit'));
+INSERT INTO ingredients (ingredient_name, type_id) VALUES ('Onion', (SELECT type_id FROM ingredient_type WHERE name = 'Vegetable'));
+INSERT INTO ingredients (ingredient_name, type_id) VALUES ('Potato', (SELECT type_id FROM ingredient_type WHERE name = 'Vegetable'));
+INSERT INTO ingredients (ingredient_name, type_id) VALUES ('Spaghetti', (SELECT type_id FROM ingredient_type WHERE name = 'Grain'));
+INSERT INTO ingredients (ingredient_name, type_id) VALUES ('Garlic', (SELECT type_id FROM ingredient_type WHERE name = 'Fruit'));
+INSERT INTO ingredients (ingredient_name, type_id) VALUES ('Olive Oil', (SELECT type_id FROM ingredient_type WHERE name = 'Other'));
+INSERT INTO ingredients (ingredient_name, type_id) VALUES ('Parmesan Cheese', (SELECT type_id FROM ingredient_type WHERE name = 'Cheese'));
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,14 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS recipe_categories;
+DROP TABLE IF EXISTS ingredients;
+DROP TABLE IF EXISTS ingredient_type;
+DROP TABLE IF EXISTS recipe_ingredient;
+DROP TABLE IF EXISTS meal_plan;
+DROP TABLE IF EXISTS meals;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,5 +17,62 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE recipes (
+    recipe_id SERIAL PRIMARY KEY,
+    recipe_name varchar(255) NOT NULL,
+    description text,
+    author int references users (user_id),
+    date_added timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY,
+    category_name varchar(50) NOT NULL
+);
+
+CREATE TABLE recipe_categories (
+    id SERIAL PRIMARY KEY,
+    recipe_id int references recipes (recipe_id),
+    category_id int references categories (category_id)
+);
+
+CREATE TABLE ingredients (
+    ingredient_id SERIAL PRIMARY KEY,
+    ingredient_name varchar(50) NOT NULL UNIQUE,
+    type_id references ingredient_type (type_id)
+);
+
+CREATE TABLE ingredient_type (
+    type_id SERIAL PRIMARY KEY,
+    type_name varchar(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE recipe_ingredient (
+    id SERIAL PRIMARY KEY,
+    recipe_id int references recipes (recipe_id),
+    ingredient_id int references ingredients (ingredient_id),
+    quantity numeric(10, 2),
+    unit varchar(50)
+);
+
+CREATE TABLE meal_plan (
+    meal_plan_id SERIAL PRIMARY KEY,
+    meal_plan_name varchar(200) NOT NULL,
+    user_id int references users (user_id),
+    creation_date timestamp DEFAULT current_timestamp,
+    start_date date NOT NULL,
+    end_date date NOT NULL
+);
+
+CREATE TABLE meals (
+    meal_id SERIAL PRIMARY KEY,
+    meal_plan_id int references meal_plan (meal_plan_id),
+    recipe_id int references recipes (recipe_id),
+    meal_date Date NOT NULL,
+    meal_type varchar(50)
+);
+
+
 
 COMMIT TRANSACTION;
